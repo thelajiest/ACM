6d7aaf3ff44e96bb1cb4c3dae777c98f0113d832
thelajiest <316200668@qq.com>
2022-10-16 19:04:29 +0800

    submit

---
+++ Atcoder/AtCoder Beginner Contest 222/H_-_Beautiful_Binary_Tree.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using i64 = long long;
+
+constexpr int mod = 998244353;
+
+template <int mod>
+struct mint {
+    int x;
+    mint() : x(0) {}
+    mint(int64_t y) : x(y >= 0 ? y % mod : (mod - (-y) % mod) % mod) {}
+    mint &operator+=(const mint &p) {
+        if ((x += p.x) >= mod) x -= mod;
+        return *this;
+    }
+    mint &operator-=(const mint &p) {
+        if ((x += mod - p.x) >= mod) x -= mod;
+        return *this;
+    }
+    mint &operator*=(const mint &p) {
+        x = (int)(1LL * x * p.x % mod);
+        return *this;
+    }
+    mint &operator/=(const mint &p) {
+        *this *= p.inverse();
+        return *this;
+    }
+    mint operator-() const { return mint(-x); }
+    mint operator+(const mint &p) const { return mint(*this) += p; }
+    mint operator-(const mint &p) const { return mint(*this) -= p; }
+    mint operator*(const mint &p) const { return mint(*this) *= p; }
+    mint operator/(const mint &p) const { return mint(*this) /= p; }
+    bool operator==(const mint &p) const { return x == p.x; }
+    bool operator!=(const mint &p) const { return x != p.x; }
+    mint inverse() const {
+        int a = x, b = mod, u = 1, v = 0, t;
+        while (b > 0) {
+            t = a / b;
+            swap(a -= t * b, b);
+            swap(u -= t * v, v);
+        }
+        return mint(u);
+    }
+    friend ostream &operator<<(ostream &os, const mint &p) { return os << p.x; }
+    friend istream &operator>>(istream &is, mint &a) {
+        int64_t t;
+        is >> t;
+        a = mint<mod>(t);
+        return (is);
+    }
+    int get() const { return x; }
+    static constexpr int get_mod() { return mod; }
+};
+using Z = mint<mod>;
+
+int main() {
+    std::ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    return 0;
+}
